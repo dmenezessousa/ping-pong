4ac3eaad6164cf64ec02a766766b5344345b5480
Diego Sousa <khesanh@Diegos-MacBook-Pro.local>
2023-07-10 13:41:45 -0400

    Get elements from html via JS

---
+++ script.js
@@ -0,0 +1,11 @@
+//Get game Elements =================================
+
+let gameArea = document.querySelector(".game-area");
+let winnerText = document.querySelector("winner-text");
+let playerPaddle = document.querySelector(".player-paddle");
+let computerPaddle = document.querySelector(".computer-paddle");
+let ball = document.querySelector(".ball");
+let playerScore = document.querySelector(".player_1_score");
+let computerScore = document.querySelector(".player_2_score");
+let startGameButton = document.querySelector(".start-btn");
+let restartGameButton = document.querySelector(".restart-btn");
